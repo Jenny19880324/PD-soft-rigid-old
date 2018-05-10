14b303dd46ead2593917fe078140c3b9937e69d7
Jing <lijing070769@hotmail.com>
2018-05-09 19:21:41 -0600

    laplacian built from triplets, precomputation time is much shorter

--- include/igl/laplacian_matrix.cpp
+++ include/igl/laplacian_matrix.cpp
@@ -23,6 +23,8 @@ IGL_INLINE void igl::laplacian_matrix(
 
 	L.resize(V.rows(), V.rows());
 	L.setZero();
+	std::vector<Triplet<Scalar>> L_triplets;
+	L_triplets.reserve(16 * F.rows());
 	int simplex_size = F.cols();
 	// 4 for tets
 	assert(simplex_size == 4);
@@ -35,16 +37,47 @@ IGL_INLINE void igl::laplacian_matrix(
 			  V(F(i, 0), 2) - V(F(i, 3), 2), V(F(i, 1), 2) - V(F(i, 3), 2), V(F(i, 2), 2) - V(F(i, 3), 2);
 		Dm_inv_trans = Dm.inverse().transpose();
 		
-		SparseMatrix<Scalar> Ai(3, V.rows());
-		vector<Triplet<Scalar>> Ai_IJV;
-		Ai_IJV.push_back(Triplet<Scalar>(0, F(i, 0), 1)); Ai_IJV.push_back(Triplet<Scalar>(0, F(i, 3), -1));
-		Ai_IJV.push_back(Triplet<Scalar>(1, F(i, 1), 1)); Ai_IJV.push_back(Triplet<Scalar>(1, F(i, 3), -1));
-		Ai_IJV.push_back(Triplet<Scalar>(2, F(i, 2), 1)); Ai_IJV.push_back(Triplet<Scalar>(2, F(i, 3), -1));
-		Ai.setFromTriplets(Ai_IJV.begin(), Ai_IJV.end());
-		SparseMatrix<Scalar> Gi(3, V.rows());
-		Gi = (Dm_inv_trans * Ai).sparseView();
-		L += Gi.transpose() * Gi;
+		//SparseMatrix<Scalar> Ai(3, V.rows());
+		//vector<Triplet<Scalar>> Ai_IJV;
+		//Ai_IJV.push_back(Triplet<Scalar>(0, F(i, 0), 1)); Ai_IJV.push_back(Triplet<Scalar>(0, F(i, 3), -1));
+		//Ai_IJV.push_back(Triplet<Scalar>(1, F(i, 1), 1)); Ai_IJV.push_back(Triplet<Scalar>(1, F(i, 3), -1));
+		//Ai_IJV.push_back(Triplet<Scalar>(2, F(i, 2), 1)); Ai_IJV.push_back(Triplet<Scalar>(2, F(i, 3), -1));
+		//Ai.setFromTriplets(Ai_IJV.begin(), Ai_IJV.end());
+		//SparseMatrix<Scalar> Gi(3, V.rows());
+		//Gi = (Dm_inv_trans * Ai).sparseView();
+		//L += Gi.transpose() * Gi;
+
+		Eigen::Vector3d v4 = -Dm_inv_trans.col(0) - Dm_inv_trans.col(1) - Dm_inv_trans.col(2);
+		for (int row = 0; row < 3; row++) {
+			for (int col = 0; col < 3; col++) {
+
+				L_triplets.push_back(Triplet<Scalar>(
+					F(i, row),
+					F(i, col),
+					Dm_inv_trans.col(row).dot(Dm_inv_trans.col(col))
+					));
+			}
+			L_triplets.push_back(Triplet<Scalar>(
+				F(i, row),
+				F(i, 3),
+				Dm_inv_trans.col(row).dot(v4)
+				));
+		}
+		for (int col = 0; col < 3; col++) {
+			L_triplets.push_back(Triplet<Scalar>(
+					F(i, 3),
+					F(i, col),
+					Dm_inv_trans.col(col).dot(v4)
+			));
+		}
+		L_triplets.push_back(Triplet<Scalar>(
+			F(i, 3),
+			F(i, 3),
+			v4.dot(v4)
+			));
 	}
+	L.setFromTriplets(L_triplets.begin(), L_triplets.end());
+	L.makeCompressed();
 }
 
 
